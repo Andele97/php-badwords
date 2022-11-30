063b49fc57c86e39da7c7a3bf91b0c0e75548059
Andele97 <andreapinna40@live.it>
2022-11-30 15:36:52 +0100

    [add] lunghezza stringa

--- pagina-di-atterraggio.php
+++ pagina-di-atterraggio.php
@@ -1,12 +1,27 @@
+<?php
+$name = $_GET['nome'];
+$lastname = $_GET['cognome'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>pagina di atterraggio</title>
 </head>
 <body>
+
+<h1>
+  Benvenuto/a <?php echo ucwords($name . ' ' . $lastname) ?> <br>
+  la stringa è lunga <?php echo strlen($name . ' ' . $lastname) ?> caratteri
+</h1>
+
+<h2>
+Benvenuto/a <?php echo ucwords($name . ' ' . $lastname) ?>
+</h2>
   
 </body>
 </html>
